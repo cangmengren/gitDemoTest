c32de6cc4763c49b96180e2e01ed181e3f220735
cangmengren <zhangyukunsvip@163.com>
2022-01-05 22:20:27 +0800

    初始化页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -17,7 +16,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin: 0;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login-bg">
+    <div>{{ welcomeMsg }}，</div>
+    <el-button type="text" @click="startDream()">
+      这里是梦启航的地方
+      <i class="el-icon-position"></i>
+      </el-button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      welcomeMsg: '欢迎来到藏梦阁',
+      loginForm: {
+        userName: '',
+        passwd: '',
+      },
+    };
+  },
+  methods: {
+    startDream() {
+      console.log('梦之旅开启');
+    },
+  },
+  mounted() {},
+};
+</script>
+<style scoped>
+.login-bg {
+  background-size: 100% 100%;
+  height: 600px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
